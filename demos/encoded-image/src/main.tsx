4d117f71cdd8e9ad63cbcce1640503ab667b84d7
chelkyl <14041823+chelkyl@users.noreply.github.com>
2024-02-28 18:30:56 -0600

    feat: switch encoded-image demo to vite

---
+++ demos/encoded-image/src/main.tsx
@@ -0,0 +1,15 @@
+import { init, render } from 'react-canvaskit'
+
+import App from './App.tsx'
+
+const htmlCanvasElement = document.createElement('canvas')
+const rootElement = document.querySelector('#root')
+if (!rootElement) {
+  throw new Error('No root element defined.')
+}
+rootElement.appendChild(htmlCanvasElement)
+htmlCanvasElement.width = 400
+htmlCanvasElement.height = 300
+
+await init()
+await render(<App />, htmlCanvasElement)
